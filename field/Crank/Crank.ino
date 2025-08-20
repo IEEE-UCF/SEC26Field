a6b17e11bc01278ac76f6e331da1afe6abc5c10d
BigManRaffa <animedudebro@gmail.com>
2025-08-19 20:15:06 -0400

    Crank fix

--- field/Crank/Crank.ino
+++ field/Crank/Crank.ino
@@ -1,9 +1,7 @@
-// Code for Antenna 2 for the Southeast Con 2026 Hardware Competition
+#include <Arduino.h>
+#include <ezButton.h>  // The library to use for SW pin
 
-/*
- * This Arduino Nano code was developed by newbiely.com
- * This Arduino Nano code is made available for public use without any restriction
- */
+#define SW_PIN 4
 
 int CLK_PIN = 2;
 int DT_PIN = 3;
@@ -12,7 +10,6 @@ int DIRECTION_CW = 0;   // clockwise direction
 int DIRECTION_CCW = 1;  // anticlockwise direction
 
 int counter = 0;
-
 int direction = DIRECTION_CW;
 int CLK_state;
 int prev_CLK_state;
@@ -22,6 +19,14 @@ int green = 10;
 int blue = 11;
 int led_pin = 13;
 
+// --- ESP32 LEDC PWM Setup ---
+const int redChannel_crank = 3;
+const int greenChannel_crank = 4;
+const int blueChannel_crank = 5;
+const int freq_crank = 5000;
+const int resolution_crank = 8;
+// --- End ESP32 Setup ---
+
 int redBrightness = 0;
 int greenBrightness = 0;
 int blueBrightness = 0;
@@ -30,15 +35,21 @@ int randomColor;
 
 void setup() {
   Serial.begin(9600);
-  randomSeed(analogRead(A0));
+  // Use a valid ADC pin for ESP32. Im using GPIO 36 again, change if you want
+  randomSeed(analogRead(36)); 
   pinMode(CLK_PIN, INPUT);
   pinMode(DT_PIN, INPUT);
   
-  pinMode(red, OUTPUT);
-  pinMode(green, OUTPUT);
-  pinMode(blue, OUTPUT);
+  // --- ESP32 LEDC PWM Initialization ---
+  ledcSetup(redChannel_crank, freq_crank, resolution_crank);
+  ledcSetup(greenChannel_crank, freq_crank, resolution_crank);
+  ledcSetup(blueChannel_crank, freq_crank, resolution_crank);
+  ledcAttachPin(red, redChannel_crank);
+  ledcAttachPin(green, greenChannel_crank);
+  ledcAttachPin(blue, blueChannel_crank);
+  // --- End ESP32 Initialization ---
+  
   pinMode(led_pin, OUTPUT);
-
   randomColor = random(1, 5);
   Serial.println(randomColor);
 
@@ -46,19 +57,12 @@ void setup() {
 }
 
 void loop() {
-  // read the current state of the rotary encoder's CLK pin
   CLK_state = digitalRead(CLK_PIN);
-
-  // If the state of CLK is changed, then pulse occurred
-  // React to only the rising edge (from LOW to HIGH) to avoid double count
   if (CLK_state != prev_CLK_state && CLK_state == HIGH) {
-    // if the DT state is HIGH
-    // The encoder is rotating in anticlockwise direction => decrease the counter
     if (digitalRead(DT_PIN) == HIGH) {
       counter--;
       direction = DIRECTION_CCW;
     } else {
-      // The encoder is rotating in clockwise direction => increase the counter
       counter++;
       direction = DIRECTION_CW;
     }
@@ -68,7 +72,6 @@ void loop() {
     if((counter == 18) || (counter == -18)){
       Serial.print("LED On");
       digitalWrite(led_pin, HIGH);
-
       if(randomColor == 1)
       {
         redBrightness = 150;
@@ -94,13 +97,15 @@ void loop() {
         greenBrightness = 0; 
       }
 
-      analogWrite(red, redBrightness);
-      analogWrite(green, greenBrightness);
-      analogWrite(blue, blueBrightness);
-      delay(3000);
+      // --- ESP32 LEDC PWM Write ---
+      ledcWrite(redChannel_crank, redBrightness);
+      ledcWrite(greenChannel_crank, greenBrightness);
+      ledcWrite(blueChannel_crank, blueBrightness);
+      // --- End ESP32 Write ---
+
+      delay(3000); // Original bad behavior preserved
     }
   }
 
-  // save last CLK state
   prev_CLK_state = CLK_state;
 }
